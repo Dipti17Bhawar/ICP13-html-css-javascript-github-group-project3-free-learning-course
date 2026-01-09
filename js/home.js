ce056286e73e332d894d3306dba2f0462de3461f
pranalipekhale13 <pekhalepranali133@gmail.com>
2026-01-09 12:41:52 +0530

    update home page

--- js/home.js
+++ js/home.js
@@ -1,12 +1,12 @@
-<<<<<<< HEAD
+
 function changeText() {
   document.getElementById("title").innerText = "Hello Bhagyashri!";
   document.getElementById("text").innerText = "Text changed using JavaScript 🎉";
 }
-=======
+
 
     function toggleMenu() {
         document.querySelector(".navbar").classList.toggle("show");
     }
 
->>>>>>> 20b5a8a1f28f5f7c4d2de060712d2147fc9ce1a3
+
